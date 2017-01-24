e87a23c97e4f0f876d42a8693a361bf6677592de
Dahang Xu <dahang.xu@rci.rogers.com>
2017-01-23 22:53:05 -0500

    update ng-bootstrap to alpha.18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routedComponents } from './app-routing.module';
-import { DataService} from './shared/data.service';
+import { DataService } from './shared/data.service';
 import { ShapeChartComponent } from './shape-chart/shape-chart.component';
 import { ReactChartComponent } from './react-chart/react-chart.component';
 import { ReactAvatarComponent } from './react-avatar/react-avatar.component';
@@ -33,7 +33,7 @@ import { GithubService } from './shared/services/github.service';
     FormsModule,
     AppRoutingModule,
     HttpModule,
-    NgbModule
+    NgbModule.forRoot()
   ],
   providers: [
     DataService,
